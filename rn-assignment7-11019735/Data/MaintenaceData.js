ad0eb8e1d4fc294dc3fb34067da450b779d821b3
Nana Ampomah Arhin <anarhin002@st.ug.edu.gh>
2024-07-08 07:00:35 -0700

    added data of the icons and texts to show how the products can be maintained

---
+++ rn-assignment7-11019735/Data/MaintenaceData.js
@@ -0,0 +1,22 @@
+export const maintenanceData = [
+  {
+    image: require("../assets/Do Not Bleach.png"),
+    id: 1,
+    text: "Do not use bleach",
+  },
+  {
+    image: require("../assets/Do Not Tumble Dry.png"),
+    id: 2,
+    text: "Do not tumble dry ",
+  },
+  {
+    image: require("../assets/Do Not Wash.png"),
+    id: 3,
+    text: "Dry clean with tetrachloroethylene",
+  },
+  {
+    image: require("../assets/Iron Low Temperature.png"),
+    id: 4,
+    text: "Iron at a maximum of 110ºC/230ºF",
+  },
+];
